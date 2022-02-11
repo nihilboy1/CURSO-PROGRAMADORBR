6dee8f18929d02959a0e9689ba3039f0fcea0944
Samuel Seve <62472120+nihilboy1455@users.noreply.github.com>
2022-02-11 12:15:37 -0300

    Fim do módulo 9

--- Módulo 9 - PROJETO (Churrascômetro)/script/código.js
+++ Módulo 9 - PROJETO (Churrascômetro)/script/código.js
@@ -1,4 +1,45 @@
-function coletar_dados(){
-    var adultos_elem = document.getElementsByName("adultos")
+
+
+function coletar() {
+    var elemento = document.getElementById("adultos")
+    var adultos = parseFloat(elemento.value)
+    if (isNaN(adultos)){
+        adultos = 0}
+   
+    var elemento = document.getElementById("criancas")
+    var criancas = parseFloat(elemento.value) / 2
+    if (isNaN(criancas)){
+        criancas = 0}
+  
+    var elemento = document.getElementById("duracao")
+    var duracao = parseFloat(elemento.value)
+    if (isNaN(duracao)){
+        duracao = 0}
+ 
+    if (duracao >= 6) {
+        var carne = (adultos + criancas) * 600
+        var cerveja = adultos * 2000
+        var refri = (adultos + criancas) * 1500
+    }
+    else {
+        var carne = (adultos + criancas) * 400
+        var cerveja = adultos * 1200
+        var refri = (adultos + criancas) * 1000
+    }
+           
     
+
+
+
+
+    var elemento = document.getElementById("resultado")
+    elemento.innerHTML = 
+    "<p> Carne: " + carne / 1000 + " Kg</p>" + 
+    "<p> Cerveja: " + cerveja + " ML</p>" + 
+    "<p> Refrigerante / Água: " + refri + " ML</p>"
+
+    
+
+
 }
+
