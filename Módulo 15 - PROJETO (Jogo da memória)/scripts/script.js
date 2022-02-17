e4f4b5e7315a953ac85e2341ff9c91c411219579
Samuel Seve <62472120+nihilboy1455@users.noreply.github.com>
2022-02-17 15:35:23 -0300

    Fim do projeto do jogo da memória

--- Módulo 15 - PROJETO (Jogo da memória)/scripts/script.js
+++ Módulo 15 - PROJETO (Jogo da memória)/scripts/script.js
@@ -1,67 +1,87 @@
 const FRONT = "card_front"
 const BACK = "card_back"
-
-let techs = [
-    "bootstrap",
-    "css",
-    "electron",
-    "firebase",
-    "html",
-    'javascript',
-    "jquery",
-    "mongo",
-    "node",
-    "react"
-]
+const CARD = 'card'
+const ICON = "icon"
 
 
-let cards = null
 
-function start_game(){
-    cards = create_cards_from_techs(techs)
-    shuffle_cards(cards)
-    console.log(cards)
-}
-
-function shuffle_cards(cards){
-    let current_index = cards.length
-    let random_index = 0
-
-    while (current_index !== 0) {
-        random_index = Math.floor(Math.random() * current_index)
-        current_index--
-
-        [cards[random_index], cards[current_index]] = [cards[current_index], cards[random_index]]
-
-    }
-    
-}
-
-function create_cards_from_techs(techs){
-    let cards = []
-    for (let tech of techs){
-        cards.push(create_pair_from_tech(tech))
-    }
-    return cards.flatMap((a) => {return a})
-}
-
-function create_pair_from_tech(tech){
-    return [
-        {
-            id:create_id_with_tech(tech),
-            icon: tech,
-            flipped: false
-        },
-        {
-            id:create_id_with_tech(tech),
-            icon: tech,
-            flipped: false
-        }
-    ]
-}
-
-function create_id_with_tech(tech){
-    return tech + parseInt(Math.random() * 1000)
-}
 
 start_game()
+
+function start_game(){
+    initialize_cards(game.create_cards_from_techs())
+}
+
+function initialize_cards(cards){
+    let game_board = document.getElementById("game_board")
+    game_board.innerHTML = ""
+    game.cards.forEach(card => {
+      let card_element = document.createElement("div")
+      card_element.id = card.id
+      card_element.classList.add(CARD)
+      card_element.dataset.icon = card.icon
+
+      create_card_content(card, card_element)
+
+      card_element.addEventListener("click", flip_card)
+      game_board.appendChild(card_element)
+    })
+
+}
+
+
+function create_card_content(card, card_element){
+    create_card_face(FRONT, card, card_element)
+    create_card_face(BACK, card, card_element)
+
+
+}
+
+function create_card_face(face, card, element){
+    let card_element_face = document.createElement("div")
+    card_element_face.classList.add(face)
+    if (face === FRONT){
+        let icon_element = document.createElement("img")
+        icon_element.classList.add(ICON)
+        icon_element.src = "imagens/" + card.icon + ".png"
+        card_element_face.appendChild(icon_element)
+    }   
+    else {
+        card_element_face.innerHTML = "&lt/&gt"
+    }
+    element.appendChild(card_element_face)
+}
+
+function flip_card(){
+    if (game.set_card(this.id)) {
+
+        this.classList.add("flip")
+        if(game.second_card){
+   
+            if(game.check_match()){
+                game.clear_cards()
+                if(game.check_game_over()){
+                    let game_over_layer = document.getElementById("game_over")
+                    game_over_layer.style.display = "flex"
+                }
+            }
+            else {
+                setTimeout(() => {
+                    let first_card_view = document.getElementById(game.first_card.id)
+                    let second_card_view = document.getElementById(game.second_card.id)
+
+                    first_card_view.classList.remove("flip")
+                    second_card_view.classList.remove("flip")
+                    game.unflip_cards()
+                },1000)
+            }
+        }
+    }
+}
+
+function restart(){
+    game.clear_cards()
+    start_game()
+    let game_over_layer = document.getElementById("game_over")
+    game_over_layer.style.display = "none"
+}
